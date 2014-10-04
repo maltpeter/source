63b563e171172a1f1313bd6ffef06b57c9c7e244
maltpeter <mta0106@gmail.com>
2014-10-04 11:09:26 -0500

    From VS

---
+++ WebAPIDemo/WebAPIDemo/Models/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPIDemo.Models
+{
+    public interface IRepository
+    {
+        IQueryable<Order> GetAllOrders();
+        IQueryable<Order> GetAllOrdersWithDetails();
+        Order GetOrder(int id);
+    }
+}
---
+++ WebAPIDemo/WebAPIDemo/Models/Repository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPIDemo.Models
+{
+    public class Repository : IRepository 
+    {
+        private WebAPIDemoContext db;
+
+        public Repository(WebAPIDemoContext db)
+        {
+            this.db = db;
+        }
+
+        public IQueryable<Order> GetAllOrders()
+        {
+            return db.Orders;
+        }
+
+        public IQueryable<Order> GetAllOrdersWithDetails()
+        {
+            return db.Orders.Include("OrderDetails");
+        }
+
+        public Order GetOrder(int id)
+        {
+            return db.Orders.Include("OrderDetails.Book").FirstOrDefault(o => o.Id == id);
+        }
+
+ 
+    }
+}
